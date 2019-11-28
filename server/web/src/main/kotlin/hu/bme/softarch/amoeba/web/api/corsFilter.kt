40734d844b6bd82169df45a82709a6feea35b723
Márkus Krisztián <markuskrisztian1@gmail.com>
2019-11-28 21:36:08 +0100

    Allow CORS requests

--- server/web/src/main/kotlin/hu/bme/softarch/amoeba/web/api/corsFilter.kt
+++ server/web/src/main/kotlin/hu/bme/softarch/amoeba/web/api/corsFilter.kt
@@ -1,2 +1,22 @@
 package hu.bme.softarch.amoeba.web.api
 
+import javax.inject.Singleton
+import javax.ws.rs.container.ContainerRequestContext
+import javax.ws.rs.container.ContainerResponseContext
+import javax.ws.rs.container.ContainerResponseFilter
+import javax.ws.rs.ext.Provider
+
+@Provider
+@Singleton
+class CorsFilter : ContainerResponseFilter {
+
+    override fun filter(request: ContainerRequestContext, response: ContainerResponseContext) {
+        response.headers.apply {
+            add("Access-Control-Allow-Origin", "*")
+            add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+            add("Access-Control-Allow-Credentials", "true")
+            add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD, PATCH")
+        }
+    }
+
+}
